d4a54c8da87263947d0440e2206c25cf684eb158
Kartikj69 <jkartik156@gmail.com>
2025-04-01 11:25:04 +0530

    fixed API Output

--- modules/secret/outputs.tf
+++ modules/secret/outputs.tf
@@ -2,3 +2,6 @@ output "secret_id" {
   description = "The ID of the Key Vault secret."
   value       = azurerm_key_vault_secret.secret.id
 }
+output "value" {
+  value = azurerm_key_vault_secret.secret.value
+}
